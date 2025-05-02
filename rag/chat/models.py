cdd7f8bf1a87ec0348155eaf8137ffdb23eb3058
mahan-rezaei <mahanrezaei859@gmail.com>
2025-05-02 17:46:19 +0330

    add model to check if file proccessed before

--- rag/chat/models.py
+++ rag/chat/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Document(models.Model):
+    file_name = models.CharField(max_length=300)
+    is_proccessed = models.BooleanField(default=False)
+
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    
