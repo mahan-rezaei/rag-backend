622c286d78d783cee12f2b2a0ce99579810fc3ed
mahan-rezaei <mahanrezaei859@gmail.com>
2025-05-02 18:07:09 +0330

    change git ignore to ignore files folder

--- rag/chat/views.py
+++ rag/chat/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 
 class ChatView(APIView):
@@ -10,4 +11,8 @@ class ChatView(APIView):
         })
 
     def post(self, request):
-        pass
+        question = request.data.get('question')
+        if not question:
+            return Response({'error': 'question is requered.'},
+                            status=status.HTTP_400_BAD_REQUEST)
+        
---
+++ rag/rag/initialize_chroma.py
@@ -0,0 +1,7 @@
+from chat.utils import store_pdf_to_chromaDB
+import os
+
+
+def initialize_chroma_from_pdf():
+    file_path = ""
+
