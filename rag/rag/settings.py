be7dd04c3380b1777e991de16a4c45cf9ef5e9c7
mahan-rezaei <mahanrezaei859@gmail.com>
2025-05-02 16:15:39 +0330

    config the project urls and instaled apps

--- rag/rag/settings.py
+++ rag/rag/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # local apps
+    'chat.apps.ChatConfig',
+
+    # thirs party apps
+    'restframework',
 ]
 
 MIDDLEWARE = [
