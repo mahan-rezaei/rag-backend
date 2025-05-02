6ba029de31a53bbad39ebe72d5199217ca5fd9c4
mahan-rezaei <mahanrezaei859@gmail.com>
2025-05-02 18:14:32 +0330

    add initializer func for pdf

--- rag/rag/initialize_chroma.py
+++
@@ -1,8 +0,0 @@
-from chat.utils import store_pdf_to_chromaDB
-import os
-
-
-def initialize_chroma_from_pdf():
-    file_path = "rag/files/farsi-11.pdf"
-    result = store_pdf_to_chromaDB(file_path)
-    print(result)
